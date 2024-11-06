9fc7667268550982e55dd0897dfaaf92231e109a
Muiz1232 <167227414+Muiz1232@users.noreply.github.com>
2024-11-06 09:38:17 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import logging
 from logging.handlers import RotatingFileHandler
 from pyrogram import Client, raw, __version__
 from flask import Flask
+import threading
+
 vj = Flask(__name__)
 
 @vj.route('/')
 def hello_world():
     return 'Hello from Tech VJ'
-    
+
 from tg.handlers import HANDLERS
 from db import repository
 from data import config
 
-
-# log config
+# Log configuration
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 root_logger = logging.getLogger()
@@ -27,17 +28,15 @@ console_handler.setFormatter(log_format)
 root_logger.addHandler(console_handler)
 
 file_handler = RotatingFileHandler(
-    filename="bot.log", maxBytes=20 * (2**20), backupCount=3, mode="D", encoding="utf-8"
+    filename="bot.log", maxBytes=20 * (2**20), backupCount=3, mode="a", encoding="utf-8"
 )
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(log_format)
 root_logger.addHandler(file_handler)
 
-
 _logger = logging.getLogger(__name__)
 
 settings = config.get_settings()
-
 
 app = Client(
     name="my_bot",
@@ -46,8 +45,10 @@ app = Client(
     bot_token=settings.telegram_bot_token,
 )
 
+def run_flask():
+    vj.run()
 
-def main():
+def run_bot():
     try:
         logging.info(
             f"The bot is up and running on Pyrogram v{__version__} (Layer {raw.all.layer})."
@@ -66,11 +67,14 @@ def main():
                     repository.update_user(tg_id=admin, admin=True)
 
         app.run()
-        vj.run()
     except Exception as e:
         logging.error(f"Error during bot execution: {e}")
 
+def main():
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.start()
 
+    run_bot()
 
 if __name__ == "__main__":
     main()
